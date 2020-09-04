262783b07de23af3b771b6ddca7161dca73c85a2
Fitri Annisa Ahlul Jannah <56632892+ftrannisa@users.noreply.github.com>
2020-09-04 23:59:26 +0700

    Create index.js

---
+++ built-in-fuction/index.js
@@ -0,0 +1,45 @@
+var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+
+// Splice
+  input.splice(4,1,);
+  input.splice(1,2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
+  input.splice(5,2, "Pria", "SMA Internasional Metro")
+  console.log(input)
+
+// Split dan Switch 
+  const hasilSplit = input[3].split("/");
+  //console.log(hasilSplit)
+  // var month = hasilSplit[1];
+  // console.log(month);
+
+switch (month = 05) {
+    case 01:
+    console.log("Januari");
+    break;
+    case 02:
+    console.log("Februari");
+    break;
+    case 03:
+    console.log("Maret");
+    break;
+    case 04:
+    console.log("April");
+    break;
+    case 05:
+    console.log("Mei");
+    break;
+    default:
+    console.log('default');
+}
+// Sort descending
+var sortDesc = input[3].split("/").sort(function(value1, value2) { return value2 - value1 });
+console.log(sortDesc); 
+
+// Join
+join = hasilSplit.join('-');
+console.log(join);
+
+// Slice
+var name = input[1];
+var nameTrue = name.slice(0,15); 
+console.log(nameTrue);
